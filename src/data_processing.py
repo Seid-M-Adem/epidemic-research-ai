3354f2b64e09bc014f633b563ddba34ab07a1260
Seid Adem <50076112+Seid-M-Adem@users.noreply.github.com>
2024-09-25 19:44:53 +0200

    Update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,1 +1,15 @@
 
+import pandas as pd
+
+def load_data(filepath):
+    """
+    Load epidemic data from a CSV file.
+    
+    Args:
+        filepath (str): Path to the CSV file.
+        
+    Returns:
+        pd.DataFrame: Loaded data.
+    """
+    data = pd.read_csv(filepath)
+    return data
